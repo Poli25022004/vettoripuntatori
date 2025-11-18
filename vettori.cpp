79286bc6e1170870ff8975fbda09f33635f4e0d9
Poli25022004 <pdimatteo@itsmeccanicabruzzo.eu>
2025-11-18 15:28:55 +0100

    vettori somma, stampa, dichiarazione

--- vettori.cpp
+++ vettori.cpp
@@ -1,36 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define LUNGHEZZA	5
+#define LUNGHEZZA	4
 
-int inserimento() {
-	int a=0;
-	printf("inserisci un un numero: %d \n");
-	scanf("%d", & a);
-	return a;
+void somma(float* a, int lunghezza, float* b, float* c) {
+	for (int indice = 0; indice < LUNGHEZZA; indice++) {
+		c[indice] = a[indice] + b[indice];
+	}
 }
-
 int main()
 {
-	int numeri[LUNGHEZZA];
-
-	int array[LUNGHEZZA] = { 1, 7, 5, 8, 2 };
-	char stringa[LUNGHEZZA] = "ciao"; //= {'c', 'i', 'a', 'o', 0};
-
-	int a;
-	int posto = 0;
-	int numero = inserimento();
-	for (; posto< LUNGHEZZA; posto+= 1)
-	//for (int indice = 0; indice < LUNGHEZZA; indice += 1)
-	{
-		
-	    numeri[posto]=inserimento(); 
+	
+	float a[LUNGHEZZA], b[LUNGHEZZA], c[LUNGHEZZA];
+		float num;
+		for (int indice = 0; indice < LUNGHEZZA; indice++) {
+			printf("inserisci un numero");
+			scanf("%f",& num);
+			a[indice] = num;
+			b[indice] = num-1;
+		}
+		somma(a, LUNGHEZZA, b, c);
+	
+	for (int indice = 0; indice < LUNGHEZZA; indice++) {
+		printf("%f\n",c[indice]);
 
 	}
-
-	//ricomincio dal primo posto
-	for (posto = 0; posto < LUNGHEZZA; posto++) {
-		printf("il numero all'interno e: %d\n", numeri[posto]);
-	}
-
 	return 0;
+
 }
